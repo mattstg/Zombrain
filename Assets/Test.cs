4b361c2e70d31fe19f330284b0f7271dc759c930
matthew st-germain <mgermain@isimtl.local>
2020-10-03 14:34:55 -0400

    TestVersion

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /*
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Bug/Balance Fixes
+    //Fix the following issues in the game
+    1) The walls are invisible, make them visible again
+    2) Everything is shoved outside the screen when the game starts, fix that issue.
+    3) Bullets do not seem to affect zombies
+    4) The code that stops zombies and humans from spawning in walls seems to be bugged
+    5) Set number of humans to spawn to 100
+   
+
+    Research
+    //Awnser the following questions with enough detail to explain to someone who hasn't seen the game or code yet
+    1) How and where is the animations for the different unit types controlled, both in code and editor? 
+        For example, there is no animation code in Player, yet it has different animations than Zombie
+    2) Explain the flow of the movement system of the Humans? At what point does the movement calculation differ from Zombies?
+    3) Who has a further detection range Humans or Zombies?
+    4) How do zombies pick thier targets to follow?
+    5) Explain the use of the out keyword and bool return type in FindClosestTarget. Why do you assume that design was used?
+    
+    New Features
+    -Make A HUD that shows the amount of ammo by showing pictures of ammo in a Horizontal layout group
+    -Make the HUD showcase the number of humans and zombies as text
+
+
+
+
+
+
+
+
+
+
+
+
+
+     */
 }
