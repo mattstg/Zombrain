11e5e1faa6f9c12d8a08fd27df1b847117d6ca5a
matthew st-germain <mgermain@isimtl.local>
2022-07-26 09:19:32 -0400

    French trans + question

---
+++ Assets/Test_FR.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    /*
+      The game is on the SampleScene scene.
+
+    Corrections de bugs/équilibrage (40 %)
+    //Résoudre les problèmes suivants dans le jeu
+    1) Les murs ne sont pas visibles pour une raison quelconque. Corrigez-le pour qu'ils soient à nouveau visibles
+    2) Tout est poussé hors de l'écran lorsque le jeu démarre, corrigez ce problème.
+    3) Définir le nombre d'humains à frayer à 100
+    4) Les balles ne semblent pas affecter les zombies, corrigez cela. (Faire cette question en dernier)
+
+    Recherche (30%)
+    // Répondez aux questions suivantes avec suffisamment de détails pour expliquer à quelqu'un qui n'a pas encore vu le jeu ou le code (Expliquez les fichiers et les lignes de code, sous forme de points)
+    // N'écrivez pas simplement une réponse en un seul mot, expliquez où se trouvent les informations, les classes et les fonctions impliquées et votre justification également.
+
+    1) Qui a une portée de détection supplémentaire Humains ou Zombies ?
+    2) Comment les zombies choisissent-ils leurs cibles à suivre ?
+    3) Lorsque le jeu commence, comment est choisi l'endroit où les zombies et les humains peuvent apparaître ? Quelles précautions de sécurité existent?
+    4) Quelle est la structure d'héritage pour Unit et toutes les choses qui sont des enfants de Unit.
+Qu'est-ce que les différents enfants remplacent ?
+    
+    Nouvelles fonctionnalités (30 %)
+    -Créez un HUD qui montre la quantité de munitions en affichant des images de munitions dans un groupe de disposition horizontale
+    
+
+
+
+     */
+}
