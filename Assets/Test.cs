5c7fc34fe4b3160b7f80d8b3b1a9790bae83f385
matthew st-germain <mgermain@isimtl.local>
2022-07-26 09:19:35 -0400

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -12,13 +12,17 @@ public class Test : MonoBehaviour
     1) The walls are not visible for some reason. Fix it so they are visible again
     2) Everything is shoved outside the screen when the game starts, fix that issue.
     3) Set number of humans to spawn to 100
-    4) Bullets do not seem to affect zombies  (Do this question last)
+    4) Bullets do not seem to affect zombies, fix this.  (Do this question last)
 
     Research (30%)
-    //Awnser the following questions with enough detail to explain to someone who hasn't seen the game or code yet
+    //Answer the following questions with enough detail to explain to someone who hasn't seen the game or code yet (Explain files and lines of code, in point form)
+    //Do not just write a single word answer, explain where the info, the classes and functions involved  and your justification as well.
+
     1) Who has a further detection range Humans or Zombies?
     2) How do zombies pick thier targets to follow?
     3) When zombies are placed, they do not overlap the walls or existing zombies. How?
+    4) When the game starts, how is the spot for zombies and humans to spawn chosen? What safety precautions exist?
+
     
     New Features (30%)
     -Make A HUD that shows the amount of ammo by showing pictures of ammo in a Horizontal layout group
